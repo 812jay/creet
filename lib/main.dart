ae67d54de36599b83e11ae45515756facc972b61
YONGJE JOE <jay@YONGJEui-MacBookPro.local>
2025-07-08 03:26:57 +0900

    [CHORE]Google Sign in 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:creet/lib/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -11,9 +15,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: HomeView(),
-    );
+    return MaterialApp(title: 'Flutter Demo', home: HomeView());
   }
 }
